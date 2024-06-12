b7e20505d599714bea4ad645c267b695838d64ef
blue-keyboard <bluekeyboard9@gmail.com>
2024-06-12 11:14:39 +0200

    add dist folder

--- src/components/Photo/Photo.js
+++ src/components/Photo/Photo.js
@@ -1,8 +1,5 @@
 import './Photo.css'
 
-// Photo by <a href="https://unsplash.com/@anniespratt?utm_source=gallery&utm_medium=referral">Annie Spratt</a>
-// on <a href="https://unsplash.com/?utm_source=gallery&utm_medium=referral">Unsplash</a>
-
 export const parsePhotos = (rawPhotos) => {
    if (!rawPhotos) return false
 
@@ -63,8 +60,6 @@ export const getPhotoDisplay = (photo, isLastPhotoInArray) => {
 }
 
 const createPhotoFullDisplayHTML = (event, photo) => {
-   console.log(event.target, photo)
-
    const fullDisplayDiv = document.createElement('div')
    fullDisplayDiv.classList.add('full-display')
 
@@ -72,52 +67,19 @@ const createPhotoFullDisplayHTML = (event, photo) => {
       <div class="full-display--wrapper">
          <img class="full-display--image" src="${photo.src}" alt="${photo.alt}" />
          <div class="full-display--links">
-            <a id="download" href="${photo.download}">Download</a>
-            <span id="author" >Photo by <a href="${photo.referalAuthor}">${photo.author}</a> in  <a href="https://unsplash.com/?utm_source=gallery&utm_medium=referral">Unsplash</a> </span>
+            <a id="download" href="#">Download</a>
+            <span id="author" >Photo by <a href="${photo.referalAuthor}" target="_blank">${photo.author}</a> in  <a href="https://unsplash.com/?utm_source=gallery&utm_medium=referral" target="_blank">Unsplash</a> </span>
          </div>
       </div>
    `
-
    fullDisplayDiv.innerHTML = PhotoDisplay
 
    document.querySelector('#app').appendChild(fullDisplayDiv)
 
+   // go back to gallery by clicking in the darkened area (outside).
    fullDisplayDiv.addEventListener('click', (event) => {
       const wrapper = document.querySelector('.full-display--wrapper')
-
-      console.log(
-         event.target,
-         fullDisplayDiv,
-         wrapper,
-         event.target === fullDisplayDiv,
-         event.target === wrapper
-      )
-
       if (fullDisplayDiv !== event.target && wrapper !== event.target) return
-
-      // const div = document.querySelector('.full-display')
-      // div.remove()
       fullDisplayDiv.remove()
    })
 }
-
-// {
-//    console.log('event listener works!, photo clicked: ', e.target)
-
-//    console.log(img.src, img.src.slice(0, -21), photo)
-
-//    img.src = img.src.slice(0, -21)
-
-//    div.classList.add('full-display')
-// }
-
-////////////// PHOTO
-// {
-//    src: 'https://images.unsplash.com/photo-1518837695005-2083093ee35b?ixid=M3w2MTU4MTV8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTcxNTg2Mzh8&ixlib=rb-4.0.3',
-//    author: 'Matt Hardy',
-//    referalAuthor:
-//       'https://unsplash.com/@matthardy?utm_source=gallery&utm_medium=referral',
-//    resize:
-//       'https://images.unsplash.com/photo-1518837695005-2083093ee35b?ixid=M3w2MTU4MTV8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTcxNTg2Mzh8&ixlib=rb-4.0.3&fit=crop&w=816&h=400',
-//    class: 'horizontal'
-// }
